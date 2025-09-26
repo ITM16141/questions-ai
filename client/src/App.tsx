ebf5d4bc65264c86f0e2d1af8d4bcdafe35ab9e2
ITM16141 <i.atsuki611@gmail.com>
2025-09-27 00:33:43 +0900

    待機中のメッセージにアニメーションを追加

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import React, { useEffect, useState } from "react";
 import DifficultySelector from "./components/DifficultySelector";
 import RangeSelector from "./components/RangeSelector";

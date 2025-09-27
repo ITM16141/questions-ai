127b20314c56bdbe3a1b6b403a1e5b52c0ab3044
ITM16141 <i.atsuki611@gmail.com>
2025-09-27 23:28:56 +0900

    書式を修正

--- server/db.ts
+++ server/db.ts
@@ -10,28 +10,28 @@ if (!fs.existsSync(dataDir)) {
 const db = new Database(path.join(dataDir, "history.db"));
 
 db.exec(`
-    CREATE TABLE IF NOT EXISTS history (
-       id TEXT PRIMARY KEY,
-       userId TEXT,
-       difficulty TEXT,
-       includeMathThree INTEGER,
-       problem TEXT,
-       solution TEXT,
-       timestamp INTEGER,
-       tags TEXT,
-       pinned INTEGER,
-       opened INTEGER,
-       views INTEGER
+     CREATE TABLE IF NOT EXISTS history (
+        id TEXT PRIMARY KEY,
+        userId TEXT,
+        difficulty TEXT,
+        includeMathThree INTEGER,
+        problem TEXT,
+        solution TEXT,
+        timestamp INTEGER,
+        tags TEXT,
+        pinned INTEGER,
+        opened INTEGER,
+        views INTEGER
     )
 `);
 
 db.exec(`
-  CREATE TABLE IF NOT EXISTS sessions (
-    id TEXT PRIMARY KEY,
-    status TEXT,
-    problem TEXT,
-    solution TEXT
-  )
+    CREATE TABLE IF NOT EXISTS sessions (
+        id TEXT PRIMARY KEY,
+        status TEXT,
+        problem TEXT,
+        solution TEXT
+    )
 `);
 
 export default db;

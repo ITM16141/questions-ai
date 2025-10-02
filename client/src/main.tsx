34b007984c27acad3804898395edc796c2fbb642
ITM16141 <i.atsuki611@gmail.com>
2025-10-02 14:26:46 +0900

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,7 @@ import GalleryPage from "./pages/GalleryPage";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
 import SharePage from "./pages/SharePage";
+import AccountPage from "./pages/AccountPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                 <Route path="/history" element={<HistoryPage />} />
                 <Route path="/gallery" element={<GalleryPage />} />
                 <Route path="/share/:id" element={<SharePage />} />
+                <Route path="/account" element={<AccountPage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
             </Routes>
